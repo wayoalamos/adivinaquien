1b7bc57f5b659a56025d274734c362e0fa87687e
martin alamos <martinalamos@martins-MacBook-Pro-2.local>
2021-11-11 17:17:01 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import logo from './silvafoto5.png';
 import './App.css';
 import React, { useState } from 'react';
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: #e91e63;
+  color: white;
+  padding: 5px 15px;
+  border-radius: 5px;
+  outline: 0;
+  text-transform: uppercase;
+  margin: 10px 0px;
+  cursor: pointer;
+  box-shadow: 0px 2px 2px lightgray;
+  transition: ease background-color 250ms;
+  &:hover {
+    background-color: #ad1457;
+  }
+  &:disabled {
+    cursor: default;
+    opacity: 0.7;
+  }
+`;
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,9 +38,9 @@ function App() {
         <h1> Adivina quien ...</h1>
         <p> {questions[count]} </p>
         <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={() => setCount(count + 1)}>
+        <Button onClick={() => setCount(count + 1)}>
           Siguiente
-        </button>
+        </Button>
         <button onClick={() => setCount(count - 1)}>
           Atras
         </button>
