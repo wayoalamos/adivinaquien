4496800058eb642ee112487dc47209334df2ceaa
martin alamos <martinalamos@martins-MacBook-Pro-2.local>
2021-11-11 16:58:51 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import logo from './silvafoto5.png';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const questions = [
+    "quien se masturbó hoy",
+    "va a ser el primero en vomitar en el matri",
+    "tiene VIH",
+    "caca"
+  ]
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Adivina quien </h1>
-        <p> Se masturbo hoy </p>
+        <h1> Adivina quien ...</h1>
+        <p> {questions[count]} </p>
         <img src={logo} className="App-logo" alt="logo" />
-        <button>SIGUIENTE</button>
+        <button onClick={() => setCount(count + 1)}>
+          Siguiente
+        </button>
       </header>
     </div>
   );
