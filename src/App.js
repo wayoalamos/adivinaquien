0f953fe8483c05b6a6b7acbfabd5dce3f8d77da1
martin alamos <martinalamos@martins-MacBook-Pro-2.local>
2021-11-11 16:35:23 -0300

    .

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1> Adivina quien </h1>
+        <p> Se masturbo hoy </p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          aAA <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
