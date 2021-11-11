df665725d5fe693e7207ef4d7763fa651b82b534
martin alamos <martinalamos@martins-MacBook-Pro-2.local>
2021-11-11 17:20:41 -0300

    .

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import React, { useState } from 'react';
 import styled from "styled-components";
 
 const Button = styled.button`
+  width: 150px;
+  height: 50px;
   background-color: #e91e63;
   color: white;
   padding: 5px 15px;
   border-radius: 5px;
   outline: 0;
   text-transform: uppercase;
-  margin: 10px 0px;
+  margin: 10px 5px;
   cursor: pointer;
   box-shadow: 0px 2px 2px lightgray;
   transition: ease background-color 250ms;
@@ -26,10 +28,10 @@ const Button = styled.button`
 function App() {
   const [count, setCount] = useState(0);
   const questions = [
+    "va a ser el primer invitado en bailar con la novia",
     "quien se masturbó hoy",
     "va a ser el primero en vomitar en el matri",
     "tiene VIH",
-    "caca"
   ]
 
   return (
